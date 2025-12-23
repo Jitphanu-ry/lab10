083726cae6fd894007cc7c00f1a6a63ac965af23
Jitphanu-ry <jitphanuriyasarn@gmail.com>
2025-12-23 15:56:55 +0700

    push1

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -10,6 +10,12 @@ int main (){
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string text;
+	dest << "-------------------- BOOM ---------------------" << endl;
+	while(getline(source,text)){
+		dest << text << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string num;
+    int num_data;
+    double sum = 0,mean,sum_square = 0,standard;
+    while(getline(source,num)){
+        sum += stod(num);
+        sum_square += pow(stod(num),2);
+        num_data++;
+    }
+    mean = sum/num_data;
+    standard = sqrt((sum_square/num_data)- pow(mean,2));
+    cout << "Number of data = " << num_data << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << standard;
+}
